ef0595d930f11d5a474d894d33fd75f7c7148dbe
robschwitzer <deadinthewater@me.com>
2018-07-02 16:45:05 -0700

    added initial nav bar component

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './components/Nav.js';
+
 
 class App extends Component {
-  state = {users: []}
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
 
   render() {
     return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
+      <Nav />
     )
   }
 }
