310e1a7dbae6fe86e992d801bad5b0070c91d087
robschwitzer <deadinthewater@me.com>
2018-07-02 20:28:32 -0700

    fixed navbar

--- web/src/components/Nav.js
+++ web/src/components/Nav.js
@@ -10,10 +10,10 @@ class Nav extends Component {
           <img width="20" src={logo} alt="clock-logo"/>
           <h1 className="text-logo">Waiting Room.</h1>
           <div className="nav-right">
-            <td className="item">For Patients</td>
-            <td className="item">For Clinics</td>
-            <td className="item">Register</td>
-            <td className="item">Login</td>
+            <div className="item">For Patients</div>
+            <div className="item">For Clinics</div>
+            <div className="item">Register</div>
+            <div className="item">Login</div>
           </div>
         </div>
       </div>
