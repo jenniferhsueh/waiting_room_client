e9508a69e3498c02742724467497bdc24667093a
robschwitzer <deadinthewater@me.com>
2018-07-03 12:21:46 -0700

    added google maps placeholder

--- web/src/components/MapView.js
+++ web/src/components/MapView.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react'
 import '../component-styles/MapView.css';
 
+const Component = () => <div></div>;
+
 class MapView extends Component {
+  static defaultProps = {
+    center: { lat: 49.2823762, lng: -123.1090515 },
+    zoom: 11
+  }
+
   render() {
     return (
       <div className="map">
+        <GoogleMapReact
+          defaultCenter={ this.props.center }
+          defaultZoom={ this.props.zoom }>
+          <Component
+            lat={ 49.2823762 }
+            lng={ -123.1090515 }
+          />
+        </GoogleMapReact>
       </div>
     )
   }
