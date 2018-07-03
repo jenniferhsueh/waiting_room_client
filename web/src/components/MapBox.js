8ba02c36ac1cb5d8e7479a717da497c24cb731b8
robschwitzer <deadinthewater@me.com>
2018-07-03 10:32:16 -0700

    added map view component structure

--- web/src/components/MapBox.js
+++ web/src/components/MapBox.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import '../component-styles/MapBox.css';
+import MapView from './MapView.js';
 
 class MapBox extends Component {
   render() {
     return (
       <div className="map-container">
-
+        <MapView />
       </div>
     )
   }
---
+++ web/src/components/MapView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../component-styles/MapView.css';
+
+class MapView extends Component {
+  render() {
+    return (
+      <div className="map">
+      </div>
+    )
+  }
+}
+
+
+export default MapView;
