39902b757f5526478fd3b26bbc654f6d1602993f
Jennifer Chang Hung Hsueh <jenhsueh15@gmail.com>
2018-07-03 16:21:33 -0700

    rendered map, markers, and user location onto mobile device

--- mobile/App.js
+++ mobile/App.js
@@ -1,65 +1,84 @@
-import React, { Component } from 'react'
-import { AppRegistry, View, Text } from 'react-native'
+import React from "react"
+import { StyleSheet, Text, View, SafeAreaView, } from "react-native"
+import Map from "./Map"
+import { Location, Permissions, MapView } from "expo"
+import YelpService from "./yelp"
 
-import List from './List'
-import Input from './Input'
-import Title from './Title'
 
-export default class App extends Component {
+const region = {
+  latitude: 49.246292,
+  longitude: -123.116226,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+}
 
+const deltas = {
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+};
+
+export default class App extends React.Component {
   state = {
-    todos: ['Click to remove', 'Learn React Native', 'Write Code', 'Ship App'],
-    users: [{
-      id: 1,
-      username: "samsepi0l"
-    }, {
-      id: 2,
-      username: "D0loresH4ze"
-    }]
+    region: null,
+    coffeeShops: []
   }
 
-  onAddTodo = (text) => {
-    const {todos} = this.state
-
-    this.setState({
-      todos: [text, ...todos],
-    })
+  componentWillMount() {
+    this.getLocationAsync();
   }
 
-  onRemoveTodo = (index) => {
-    const {todos} = this.state
+  getClinics = async () => {
+    const { latitude, longitude } = this.state.region;
+    const userLocation = { latitude, longitude };
+    const coffeeShops = await YelpService.getClinics(userLocation);
+    this.setState({ coffeeShops });
+  };
 
-    this.setState({
-      todos: todos.filter((todo, i) => i !== index),
-    })
+  getLocationAsync = async () => {
+    // Code Below asks to get user location and then
+    // sets the region to the latitude and 
+
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied'
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    const region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      ...deltas
+    };
+    await this.setState({ region });
+    await this.getClinics();
   }
 
   render() {
-    const {todos, users} = this.state
-
+    const { region, coffeeShops } = this.state;
+/*show a preloader*/
+    
     return (
-      <View>
-        <Title>
-          To-Do List
-        </Title>
-        <Input
-          placeholder={'Type a todo, then hit enter!'}
-          onSubmitEditing={this.onAddTodo}
-        />
-        <List
-          list={todos}
-          onPressItem={this.onRemoveTodo}
-        />
-        <Title>
-          Users
-        </Title>
-        {users.map( (user) => <Text key={user.id}>{user.username}</Text> )}
-        {/* <List
-          {users.map( (user) => {
-            <Text>user.name</Text>
-          })}
-        /> */}
-      </View>
-    )
+      <SafeAreaView style={styles.container}>
+        <Map region={region} places={coffeeShops} />
+      </SafeAreaView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    left: 0,
+    right:0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute'
+  }
+})
--- mobile/index.js
+++ mobile/index.js
@@ -1,6 +1,6 @@
-import { AppRegistry, View } from 'react-native'
+import { AppRegistry, View } from "react-native"
 
 // Import the App container component
-import App from './App'
+import App from "./App"
 
-AppRegistry.registerComponent('App', () => App)
+AppRegistry.registerComponent("App", () => App)
