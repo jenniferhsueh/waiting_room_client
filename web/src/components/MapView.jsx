e26df0d5dd5905ae369a87cbb5bd6bddf6c0273b
robschwitzer <deadinthewater@me.com>
2018-07-08 16:30:14 -0700

    added wait time displays on map

--- web/src/components/MapView.jsx
+++ web/src/components/MapView.jsx
@@ -40,7 +40,18 @@ class MapView extends Component {
             return (
               <Marker key={clinic.id} latitude={clinic.coordinates.latitude} longitude={clinic.coordinates.longitude} offsetLeft={-20} offsetTop={-10} openModal={() => this.onListItemClick(clinic)} item={clinic} >
 
-              <div onClick={() => this.onListItemClick(clinic)} ><span style={{fontSize:46}} role="img">⚰️</span></div>
+              <div
+                classname="time-card"
+                onClick={() => this.onListItemClick(clinic)} >
+                <div className="wait-time-marker">{ clinic.wait_time } mins</div>
+                  <span
+                    style={{
+                      fontSize:30,
+                      color:'#5FB3D0'
+                    }}
+                    role="img"><i class="fas fa-map-marker-alt"></i>
+                  </span>
+                </div>
               </Marker>);
           })}
         </ReactMapGL>
