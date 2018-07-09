b18fd2f28e29191400df40eecd11dcfbdcd607ed
ChibweMw <caleidosymmetry@gmail.com>
2018-07-08 17:24:55 -0700

    users route created and tested on backend

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,9 +1,57 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/api/users', (req, res) => {
+	const users = [
+		{
+      first_name: "Jennifer", 
+      last_name: "Hsueh", 
+      phone: 6043151860, 
+      email: "jenn@jenn.com", 
+      password: "jenn",
+      deposit: false,
+      location: {
+        address1:"128 W Hastings St",
+        address2: "",
+        address3: "",
+        city: "Vancouver",
+        state: "BC",
+        Zipcode: "V6B 1G8",
+        country: "Canada",
+      },
+      coordinates: {
+        lat: "41.864128", 
+        long: "-87.669449"
+      },
+      clinics_id: 1
+    },
+    {
+      first_name: "Joel", 
+      last_name: "Shinness", 
+      phone: 6043151860, 
+      email: "joel@jenn.com", 
+      password: "joel",
+      deposit: true,
+      location: {
+        address1:"534 W Pender St",
+        address2: "",
+        address3: "",
+        city: "Vancouver",
+        state: "BC",
+        Zipcode: "V6B 1V3",
+        country: "Canada",
+      },
+      coordinates: {
+        lat: "49.283599", 
+        long: "-123.113811"
+      },
+      clinics_id: 2
+    }
+	];
+	res.json(customers);
 });
 
-module.exports = router;
+
+const port = 8080;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var PORT = 8080
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
