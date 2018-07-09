8d95b0ed189cc5e7be5edca64411ce0a5639e56a
robschwitzer <deadinthewater@me.com>
2018-07-09 13:58:37 -0700

    adding users to db upon register

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -39,8 +39,43 @@ module.exports = (knex) => {
 
 
     router.post("/new", (req, res, next) => {
-        // {email, password} = req.body
-        console.log("OUR NEW USER =====>", req.body)
+      let newUser;
+
+      if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.pw || !req.body.confirmPw || req.body.pw !== req.body.confirmPw) {
+        res.status(400).send(console.log("youre a little beee yatch"))
+
+      } else {
+        knex('users')
+        .insert(
+          newUser = {
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            phone: null,
+            email: req.body.email,
+            password: req.body.pw,
+            deposit: false,
+            location: {
+              address1:"",
+              address2: "",
+              address3: "",
+              city: "",
+              state: "",
+              Zipcode: "",
+              country: "",
+            },
+            coordinates: {
+              lat: "",
+              long: ""
+            },
+            clinics_id: null
+          }
+        )
+        .then(() => console.log("WE DEEEED IIIIIT", newUser))
+        .catch((e) => {
+          console.log("====>", e)
+           res.status(500).send(e);
+        })
+      }
     });
 
  return router
